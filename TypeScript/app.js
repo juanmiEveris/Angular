91e91898ef162f037c281362309312185df67e0f
Unknown <juan.miguel.garcia.gonzalez@everis.com>
2018-10-18 22:45:29 +0200

    Initial commit

---
+++ TypeScript/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var mensaje = "hola";
+if (true) {
+    var mensaje = "adios";
+}
+console.log(mensaje);
+var nombre = "Peter";
+var numero = 123;
+var booleano = true;
+var hoy = new Date();
+hoy = new Date('2020-10-21');
+console.log(hoy);
+var texto = "Hola,\n" + nombre + " " + numero;
+console.log(texto);
---
+++ TypeScript/app.ts
@@ -0,0 +1,23 @@
+var mensaje = "hola";
+
+if (true) {
+
+  var mensaje = "adios";
+}
+
+console.log(mensaje);
+
+let nombre:string = "Peter";
+let numero:number = 123;
+let booleano:boolean = true;
+
+let hoy:Date = new Date();
+
+hoy = new Date ('2020-10-21');
+
+console.log(hoy);
+
+let texto = `Hola,
+${ nombre} ${ numero}`;
+
+console.log(texto);
